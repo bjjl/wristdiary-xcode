99a6ccad60a7a2b7b9c5721e5122f4d3181c1b3e
Benjamin Lorenz <ben@pocket.services>
2021-05-28 15:48:33 +0200

    Initial Commit

---
+++ wristdiary WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wristdiary WatchKit Extension
+//
+//  Created by Benjamin Lorenz on 28.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wristdiary WatchKit Extension/wristdiaryApp.swift
@@ -0,0 +1,19 @@
+//
+//  wristdiaryApp.swift
+//  wristdiary WatchKit Extension
+//
+//  Created by Benjamin Lorenz on 28.05.21.
+//
+
+import SwiftUI
+
+@main
+struct wristdiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
